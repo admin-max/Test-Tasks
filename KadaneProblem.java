617c5593a6d49f71ee3bb0293678ca1b95569eb7
admin-max <erste1max@gmail.com>
2018-04-22 22:20:08 -0500

    Kadane Problem aligned

--- KadaneProblem.java
+++ KadaneProblem.java
@@ -8,32 +8,29 @@ package testTasks;
 public class KadaneProblem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int [] arr = { -2, 0, 1, 3, 4, 0, 5, 2, -2, 9, -1, 0};
-		
-		System.out.println(kadaneAlgorithm(arr));
+
+		int[] arr = { -2, 0, 1, 3, -100, 4, 0, 5, -5, 2, -2, 9, -1, 0 };
+
+		System.out.println(kadaneAlgorithm(arr)); // result: 13
 
 	}
-	
-	public static int kadaneAlgorithm (int [] arr){
-		
+
+	public static int kadaneAlgorithm(int[] arr) {
+
 		int maxForNow = 0;
 		int maxEndPoint = 0;
-		
-		for (int i = 0; i < arr.length; i++){
-			
+
+		for (int i = 0; i < arr.length; i++) {
+
 			maxEndPoint += arr[i];
-			
+
 			if (maxEndPoint < 0)
 				maxEndPoint = 0;
-			
+
 			if (maxForNow < maxEndPoint)
 				maxForNow = maxEndPoint;
 		}
 		return maxForNow;
-					
-		}
+
 	}
-
-
+}
